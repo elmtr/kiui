c003fc7cfd4fadb3159b2a1104c72047903505be
Soare Catalin <stevensuncatalin369@gmail.com>
2022-10-12 06:04:25 +0300

    added some structure to the navbar component. not really styled anything yet.

--- NavBar.svelte
+++ NavBar.svelte
@@ -1,19 +1,94 @@
 <script>
   import {link} from 'svelte-spa-router'
+
+  export let location
 </script>
 
-<div id="navbar">
-  <a href="/" use:link>home</a>
-  <a href="/timetable" use:link>orar</a>
-  <a href="/profile" use:link>profil</a>
+<div id="container">
+  <div id="navbar">
+    <div class="icon-container">
+      <a href="/" use:link>
+        {#if location === "home"}
+          <div class="icon icon-selected">
+
+          </div>
+        {:else} 
+          <div class="icon icon-not-selected">
+
+          </div>
+        {/if}
+      </a>
+    </div>
+    <div class="icon-container">
+      <a href="/timetable" use:link>
+        {#if location === "timetable"}
+          <div class="icon icon-selected">
+
+          </div>
+        {:else} 
+          <div class="icon icon-not-selected">
+
+          </div>
+        {/if}
+      </a>
+    </div>
+    <div class="icon-container">
+      <a href="/profile" use:link>
+        {#if location === "profile"}
+          <div class="icon icon-selected">
+
+          </div>
+        {:else} 
+          <div class="icon icon-not-selected">
+
+          </div>
+        {/if}
+      </a>
+    </div>
+  </div>
 </div>
 
 <style scoped>
-  #navbar {
+  #container {
     width: 100%;
     height: 60px;
     background: var(--gray);
     position: fixed;
     bottom: 0;
   }
+
+  #navbar {
+    width: 100%;
+    height: 100%; 
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .icon-container {
+    flex: 33%;
+    position: relative;
+  }
+
+  .icon {
+    width: 100%;
+    height: 40px;
+    background: red;
+
+    position: absolute;
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+  }
+
+  .icon-selected {
+    background: green;
+  }
+
+  .icon-not-selected {
+    background: purple;
+  }
+
+
+
+
 </style>
